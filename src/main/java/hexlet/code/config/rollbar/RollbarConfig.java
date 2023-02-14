37941660ebd9114ecd78cab9dfdb5e6855932a9a
s-chepurnov <Kitaec_31337>
2023-02-14 23:51:41 +0700

    rollbar

---
+++ src/main/java/hexlet/code/config/rollbar/RollbarConfig.java
@@ -0,0 +1,38 @@
+package hexlet.code.config.rollbar;
+
+import com.rollbar.notifier.Rollbar;
+import com.rollbar.notifier.config.Config;
+import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
+//5bd80aea97864264a0a8dc041206f5d8
+@Configuration
+@ComponentScan({"hexlet.code"})
+public class RollbarConfig {
+
+    @Value("${rollbar_token:}")
+    private String rollbarToken;
+
+    @Value("${spring.profiles.active:}")
+    private String activeProfile;
+
+    /**
+     * Register a Rollbar bean to configure App with Rollbar.
+     */
+    @Bean
+    public Rollbar rollbar() {
+
+        return new Rollbar(getRollbarConfigs(rollbarToken));
+    }
+
+    private Config getRollbarConfigs(String accessToken) {
+
+        return RollbarSpringConfigBuilder.withAccessToken(accessToken)
+                .environment("production")
+                .enabled(activeProfile.equals("prod"))
+                .build();
+    }
+}
