e97823311b4ce821dac692a13d7c08148fc9b77e
s-chepurnov <Kitaec_31337>
2023-02-15 14:12:28 +0700

    checkstyle

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -14,12 +14,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SecurityScheme(name = "javainuseapi", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class AppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/test/java/hexlet/code/AppApplicationTests.java
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -3,12 +3,11 @@ package hexlet.code;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 class AppApplicationTests {
 
-	@Test
-	void init() {
-		assertThat(true).isTrue();
-	}
+    @Test
+    void init() {
+        assertThat(true).isTrue();
+    }
 
 }
